eaf5fa8e993c9c60f9945a5760966cb1114e029a
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2020-07-11 05:18:43 -0600

    Moved File

---
+++ scripts/blocks/magnetodrill.js
@@ -0,0 +1,23 @@
+
+
+const magnetodrill = extendContent(Drill, "magneto-drill", {
+
+
+
+
+  setStats() {
+    this.super$setStats();
+    //this.stats.add(BlockStat.drillTier, 5);
+    //this.stats.add(BlockStat.drillSpeed, 250);
+    //this.stats.add(BlockStat.size, 3, true);
+
+    //this.stats.add(BlockStat.output, this.outputLiquid.liquid, this.outputLiquid.amount, false);
+
+    //this.stats.add(BlockStat.boostEffect, 2.25, StatUnit.timesSpeed);
+  }
+
+
+
+});
+
+//magnetodrill.consumes.liquid(Liquids.slag, (5/18)).boost();
